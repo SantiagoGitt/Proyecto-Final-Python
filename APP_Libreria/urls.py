f07b2358afbe1d1ec0c732b8409ead34635c1828
Tamara FS <tamufs.tw@gmail.com>
2022-09-04 18:52:43 -0300

    Libreria1

--- APP_Libreria/urls.py
+++ APP_Libreria/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from APP_Libreria.views import *
 
 urlpatterns = [
-    path("", inicio),
-    path("clientes/", clientes),
-    path("empleados/", empleados),
-    path("stock/", stock),
-    path("resenia/", resenia),
+    path("", inicio, name="inicio"),
+    path("clientes/", clientes, name="clientes"),
+    path("empleados/", empleados, name="empleados"),
+    path("stock/", stock, name="stock"),
+    path("resenia/", resenia, name="resenia"),
 
 ]
